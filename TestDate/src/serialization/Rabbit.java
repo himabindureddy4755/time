641a02ff27ee737de595cb8f51898d4f98fc8a89
hp <hp@DESKTOP-9MT34QV>
2021-04-07 14:56:21 +0530

    commit

---
+++ TestDate/src/serialization/Rabbit.java
@@ -0,0 +1,13 @@
+package serialization;
+
+import java.io.Serializable;
+
+public class Rabbit extends Animal implements Serializable{
+	int j=20;
+	int k=30;
+	public Rabbit()
+	{
+		System.out.println("rabbits no arg constructor called");
+	}
+
+}
